e6610ab5d0f2805e631bafff2f5d734099187ad6
SakaevaDiana <sakaevadiana2007@gmail.com>
2025-05-27 19:48:23 +0300

    Create 2.c

---
+++ 2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    float height;
+    float tallest = 0, secondTallest = 0, thirdTallest = 0;
+
+    printf("Введите рост учеников (отрицательное значение для завершения):\n");
+
+    while (1) {
+        scanf("%f", &height);
+        if (height < 0) break;
+
+        if (height > tallest) {
+            thirdTallest = secondTallest;
+            secondTallest = tallest;
+            tallest = height;
+        } else if (height > secondTallest) {
+            thirdTallest = secondTallest;
+            secondTallest = height;
+        } else if (height > thirdTallest) {
+            thirdTallest = height;
+        }
+
+        printf("Рост в порядке убывания:\n");
+        printf("%.2f\n", tallest);
+        if (secondTallest > 0) {
+            printf("%.2f\n", secondTallest);
+        }
+        if (thirdTallest > 0) {
+            printf("%.2f\n", thirdTallest);
+        }
+    }
+
+    return 0;
+}
